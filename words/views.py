45674f3730bf4a03ad4f4503742a133cdcc05885
unknown <akhilkashyap16@gmail.com>
2023-05-02 00:27:17 +0530

    Make computer generate a random fancy word

--- words/views.py
+++ words/views.py
@@ -2,22 +2,24 @@ from django.shortcuts import render
 from django.http import JsonResponse
 import os 
 from .models import Word
-from . word_utils import is_valid_word, is_fancy_word
+from . word_utils import is_valid_word, is_fancy_word, comp_response
 # Create your views here.
 
 def home(request):
 
     if request.method == 'POST':
+        computer_word = "waiting..."
         current_word = request.POST.get('current_word', '')
         print("Entered word is: ", current_word)
         if is_valid_word(current_word):
+            computer_word = comp_response()
             if is_fancy_word(current_word):
                 message = "valid and fancy"
             else:
                 message = "valid but not fancy"
         else:
             message = "invalid"
-        return render(request, 'home.html', {'current_word':current_word, 'message':message})
+        return render(request, 'home.html', {'current_word':current_word, 'computer_word':computer_word,'message':message})
 
     else:
         current_word = "undetected"
--- words/word_utils.py
+++ words/word_utils.py
@@ -1,20 +1,31 @@
 import os 
+import random
 
 parent_dir = os.path.dirname(os.path.abspath(__file__))
 enable_path = os.path.join(parent_dir, 'data', 'enable.txt')
 common_word_path = os.path.join(parent_dir, 'data', 'common_words.txt')
+
+with open(enable_path, 'r') as f:
+    all_words = set(line.strip() for line in f)
+
 def is_valid_word(word):
-    with open(enable_path, 'r') as f:
-        all_words = set(line.strip() for line in f)
     
     return word.lower() in all_words 
 
 
+with open(common_word_path, 'r') as f:
+    
+    common_words = set(line.strip() for line in f)
+
 # A fancy word is one that is outside of the top 10k commonly used words.
 def is_fancy_word(word):
 
-    with open(common_word_path, 'r') as f:
-        
-        common_words = set(line.strip() for line in f)
-
     return not word.lower() in common_words
+
+
+def comp_response():
+
+    fancy_words = list(set(all_words) - set(common_words))
+    ind = random.randint(0, len(fancy_words))
+
+    return fancy_words[ind]

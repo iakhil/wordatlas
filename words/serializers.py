6c90a3e30a55b3f13f97dce619cd35eaca72b8d9
unknown <akhilkashyap16@gmail.com>
2023-10-14 01:19:53 +0530

    Add auth token to local storage

--- words/serializers.py
+++ words/serializers.py
@@ -1,5 +1,14 @@
 from rest_framework import serializers 
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from .models import Bookmark
+
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+
+    def validate(self, attrs):
+        data = super().validate(attrs)
+        data.update({'username': self.user.username})
+        return data
 
 class BookmarkSerializer(serializers.ModelSerializer):
     class Meta:
--- words/views.py
+++ words/views.py
@@ -15,6 +15,12 @@ from rest_framework import viewsets
 from .models import Bookmark
 from .serializers import BookmarkSerializer
 from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.views import TokenObtainPairView
+from .serializers import CustomTokenObtainPairSerializer
+
+class CustomTokenObtainPairView(TokenObtainPairView):
+    serializer_class = CustomTokenObtainPairSerializer
+
 
 # Login View
 class LoginView(LoginView):

345622e7944962bc28ee6eb71f8038377bd31c81
unknown <akhilkashyap16@gmail.com>
2024-07-28 01:25:59 +0530

    Edit database path

--- wordatlas/settings.py
+++ wordatlas/settings.py
@@ -92,7 +92,7 @@ WSGI_APPLICATION = 'wordatlas.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': '/home/iakhil/WordAtlas/db.sqlite3',
     }
 }
 
